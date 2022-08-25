dddac1fb5d30242d50121779429d96a6d4e8554d
EnDeRIuaI <wolk93@ukr.net>
2022-08-25 19:54:40 +0300

    first commit

--- client/main.js
+++ client/main.js
@@ -1,26 +1,26 @@
-class Configurations {
+class Default {
     static webServer = 'http://localhost:8000';
 }
 
 class getDataServer {
-    static getPerson() {return fetch(Configurations.webServer + '/person').then(resp => resp.json()).catch(err => {console.error(err)});}
-    static getName() {return fetch(Configurations.webServer + '/person/name').then(resp => resp.json()).catch(err => {console.error(err)});}
-    static getData() {return fetch(Configurations.webServer + '/person?name&age&surname').then(resp => resp.json()).catch(err => {console.error(err)});}
-    static getAddress() {return fetch(Configurations.webServer + '/person/address').then(resp => resp.json()).catch(err => {console.error(err)});}
-    static getPost() {return fetch(Configurations.webServer + '/person/post/recipient').then(resp => resp.json()).catch(err => {console.error(err)});}
+    static getPerson() {return fetch(Default.webServer + '/person').then(resp => resp.json()).catch(err => {console.error(err)});}
+    static getName() {return fetch(Default.webServer + '/person/name').then(resp => resp.json()).catch(err => {console.error(err)});}
+    static getData() {return fetch(Default.webServer + '/person?name&age&surname').then(resp => resp.json()).catch(err => {console.error(err)});}
+    static getAddress() {return fetch(Default.webServer + '/person/address').then(resp => resp.json()).catch(err => {console.error(err)});}
+    static getPost() {return fetch(Default.webServer + '/person/post/recipient').then(resp => resp.json()).catch(err => {console.error(err)});}
 }
 
-let [buttonPerson, buttonName, buttonData, buttonAddress, buttonPost] = [document.querySelector('#get-person'),
-                                                                         document.querySelector('#get-name'),
-                                                                         document.querySelector('#get-data'),
-                                                                         document.querySelector('#get-address'),
-                                                                         document.querySelector('#get-post')];
+let [buttonPerson, buttonName, buttonData, buttonAddress, buttonPost] = [document.getElementById('btn_person'),
+                                                                         document.getElementById('btn_name'),
+                                                                         document.getElementById('btn_data'),
+                                                                         document.getElementById('btn_address'),
+                                                                         document.getElementById('btn_post')];
 
 
 buttonPerson.addEventListener('click', () => {
     getDataServer.getPerson()
     .then(person => {
-            document.querySelector('.person').innerHTML = JSON.stringify(person);
+            document.getElementById('person').innerHTML = JSON.stringify(person);
         }).catch(err => {
             console.error(err)
         });
@@ -29,7 +29,7 @@ buttonPerson.addEventListener('click', () => {
 buttonName.addEventListener('click', () => {
     getDataServer.getName()
         .then(person => {
-            document.querySelector('.name').innerHTML = JSON.stringify(person);
+            document.getElementById('name').innerHTML = JSON.stringify(person);
         })
         .catch(err => {
             console.error(err)
@@ -39,7 +39,7 @@ buttonName.addEventListener('click', () => {
 buttonData.addEventListener('click', () => {
     getDataServer.getData()
         .then(person => {
-            document.querySelector('.data').innerHTML = JSON.stringify(person);
+            document.getElementById('data').innerHTML = JSON.stringify(person);
         })
         .catch(err => {
             console.error(err)
@@ -49,7 +49,7 @@ buttonData.addEventListener('click', () => {
 buttonAddress.addEventListener('click', () => {
     getDataServer.getAddress()
         .then(person => {
-            document.querySelector('.address').innerHTML = JSON.stringify(person);
+            document.getElementById('address').innerHTML = JSON.stringify(person);
         })
         .catch(err => {
             console.error(err)
@@ -59,7 +59,7 @@ buttonAddress.addEventListener('click', () => {
 buttonPost.addEventListener('click', () => {
     getDataServer.getPost()
         .then(person => {
-            document.querySelector('.post').innerHTML = JSON.stringify(person);
+            document.getElementById('post').innerHTML = JSON.stringify(person);
         })
         .catch(err => {
             console.error(err)
